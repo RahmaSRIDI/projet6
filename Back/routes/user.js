ba4eef8b00e8b85c5b6c96de1fa735a29fd18cac
rahmasridi <rahmoucha@gmail.com>
2022-10-07 07:50:03 +0200

    clean project

--- Back/routes/user.js
+++ Back/routes/user.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const userCtrl = require('../controllers/user');
-const auth = require('../middleware/auth');
+
 
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
-router.get('/sauces',auth, userCtrl.login);
+
 
 module.exports = router;
